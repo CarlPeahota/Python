dcae445109bb789666294be6522619cd1d54e416
CarlPeahota <carlpeahota@gmail.com>
2016-12-28 11:39:02 -0500

    New Python Example

---
+++ PythonExamples_1.py
@@ -0,0 +1,16 @@
+print('Hello World')
+print('Hello Carl')
+print('Hello Kevin')
+print('Hello Steve')
+
+print(2+2)
+print(5-3)
+
+print("Ada Lovelace\'s", "birthday was on", "November 27, 1852")
+
+print("Ada Lovelace\'s", "birthday was on", "November 27, 1852 and\n", "she was pretty")
+print("Ada Lovelace\'s", "birthday was on", "November 27, 1852 and", "\nshe was pretty")
+print("Hello\nWorld!")
+print(int("2") + int("5"))
+
+print(float(input("Enter a number: ")) + float(input("Enter another number: ")))
